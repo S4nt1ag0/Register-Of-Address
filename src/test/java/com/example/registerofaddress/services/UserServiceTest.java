71a004d2e6b729b8253f803ef5dafcc8be4d262b
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-03-01 00:06:25 -0300

    some test of integrations in Service

--- src/test/java/com/example/registerofaddress/services/UserServiceTest.java
+++ src/test/java/com/example/registerofaddress/services/UserServiceTest.java
@@ -3,8 +3,14 @@ package com.example.registerofaddress.services;
 import com.example.registerofaddress.entities.Endereco;
 import com.example.registerofaddress.entities.User;
 import com.example.registerofaddress.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
 
 public class UserServiceTest {
 
@@ -14,8 +20,37 @@ public class UserServiceTest {
     @Mock
     UserRepository userRepository;
 
-    Endereco endereco = new Endereco();
+    Endereco endereco;
 
-    User user = new User();
+    User user;
+
+    @BeforeEach
+    void setUp() throws Exception
+    {
+        MockitoAnnotations.openMocks(this);
+        endereco = new Endereco();
+
+        endereco.setCep("01001-000");
+        endereco.setNumeroCasa(39);
+
+        user = new User();
+        user.setName("Sant");
+        user.setCPF(458447L);
+        user.setEndereco(endereco);
+    }
+
+    @Test
+    void createUsers(){
+        User user = userService.insert(this.user);
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals("Praça da Sé",user.getEndereco().getLogradouro());
+    }
+
+    @Test
+    void listOfUsers(){
+        List<User> lists = userService.findAll();
+        Assertions.assertNotNull(lists);
+        Assertions.assertEquals("01001-000",lists.get(0).getEndereco().getCep());
+    }
 
 }
